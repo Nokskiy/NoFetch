364c4ede11539a6b501c49e3359c9caa2487cf43
Nokskiy <145178078+mishaKan@users.noreply.github.com>
2025-03-03 20:20:45 +0500

    help

--- src/NoFetch/NoFetch.cpp
+++ src/NoFetch/NoFetch.cpp
@@ -1,20 +1,80 @@
-﻿// NoFetch.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <Windows.h>
+#include <conio.h>
+#include <stdio.h>
+#include <map>
+using namespace std;
 
-#include <iostream>
+HANDLE hConsole;
+
+map<string, int> colors;
+string commandList[] = {"help"};
+
+void SetTextColor(int Color);
+void command();
+void init();
+void help();
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    init();
+
+    SetTextColor(colors["Fetch"]);
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+void SetTextColor(int Color)
+{
+    SetConsoleTextAttribute(hConsole, Color);
+}
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+void command()
+{
+    string command;
+    SetTextColor(colors["Fetch"]);
+    cout << "enter the command : ";
+    SetTextColor(colors["Command"]);
+    cin >> command;
+    int commandIndex = -1;
+    for (int i = 0; i < size(commandList); i++)
+    {
+        if (command == commandList[i])
+        {
+            commandIndex = i;
+            break;
+        }
+    }
+    if (commandIndex == -1)
+    {
+
+    }
+    switch (commandIndex)
+    {
+    case(0): // help
+        help();
+        break;
+    default:
+        cout << "error, check the correctness of the command form. To do this, enter 'help'";
+        break;
+    }
+}
+
+void init()
+{
+    colors["Fetch"] = 3;
+    colors["Command"] = 2;
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    SetTextColor(colors["Fetch"]);
+    cout << "You can enter the command 'help' for help.\n";
+
+    command();
+}
+void help()
+{
+    SetTextColor(colors["Fetch"]);
+    cout << "\ncommands list: \n\n";
+    for (int i = 0; i < size(commandList); i++)
+    {
+        cout << commandList[i] << "\n\n";
+    }
+}
