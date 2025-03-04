83d1cba2923b032496729b7fb678c7e4912c983c
Nokskiy <145178078+mishaKan@users.noreply.github.com>
2025-03-04 20:57:31 +0500

    buffer

--- src/NoFetch/NoFetch.cpp
+++ src/NoFetch/NoFetch.cpp
@@ -3,12 +3,17 @@
 #include <conio.h>
 #include <stdio.h>
 #include <map>
+#include <fstream>
+#include <string>
+
 using namespace std;
 
 HANDLE hConsole;
 
 map<string, int> colors;
-string commandList[] = {"help","sys_char","monitor_avail_RAM","clear"};
+string commandList[] = {"help","sys_char","monitor_avail_RAM","clear","o_buf","e_buf" };
+
+
 
 void setTextColor(int Color);
 void command();
@@ -21,11 +26,12 @@ float usedRAM();
 void monitorAvailableRAM();
 string os();
 void clear();
+void openBuffer();
+void extendBuffer();
 
 int main()
 {
     init();
-
     setTextColor(colors["Fetch"]);
 }
 
@@ -67,6 +73,12 @@ void command()
         break;
     case(3)://clear
         clear();
+        break;
+    case(4)://open buffer
+        openBuffer();
+        break;
+    case(5)://extend buffer
+        extendBuffer();
         break;
     default:
         setTextColor(colors["Error"]);
@@ -161,5 +173,44 @@ string os()
 void clear()
 {
     system("cls");
+    command();
+}
+
+void openBuffer()
+{
+    setTextColor(colors["Fetch"]);
+    ofstream bufMake;
+    bufMake.open("buffer.txt", ios::in | ios::out);
+    bufMake.close();
+    ifstream buffer;
+    buffer.open("buffer.txt");
+    string str;
+    int i = 0;
+    while (!buffer.eof())
+    {
+        i++;
+        str = "";
+        getline(buffer, str);
+        if (str == "")
+        {
+            break;
+        }
+        cout << i << "=====> " << str << "\n";
+    }
+    buffer.close();
+    command();
+}
+
+void extendBuffer()
+{
+    setTextColor(colors["Fetch"]);
+    ofstream bufMake;
+    bufMake.open("buffer.txt", ofstream::app);
+    string text;
+    cout << "text: ";
+    cin.ignore();
+    getline(cin, text);
+    bufMake << text + "\n";
+    bufMake.close();
     command();
 }
